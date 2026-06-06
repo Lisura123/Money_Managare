b53138b075d411cd69b244c2cb21d8ff0ac79862
Lisura Sigera <IT23295360@my.sliit.lk>
2026-06-06 20:37:15 +0530

    Fix Keychain: use UserDefaults on simulator, fix entitlements bundle ID

--- ios/Runner/Services/KeychainService.swift
+++ ios/Runner/Services/KeychainService.swift
@@ -4,41 +4,58 @@ import Security
 enum KeychainError: Error { case saveFailed, readFailed, deleteFailed }
 
 struct KeychainService {
+
+    // MARK: - Public API
+
     static func save(_ value: String, for key: String) throws {
+#if targetEnvironment(simulator)
+        UserDefaults.standard.set(value, forKey: key)
+#else
         let data = Data(value.utf8)
         let query: [CFString: Any] = [
-            kSecClass:            kSecClassGenericPassword,
-            kSecAttrAccount:      key,
-            kSecAttrAccessible:   kSecAttrAccessibleAfterFirstUnlock,
-            kSecValueData:        data
+            kSecClass:          kSecClassGenericPassword,
+            kSecAttrAccount:    key,
+            kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
+            kSecValueData:      data
         ]
         SecItemDelete(query as CFDictionary)
         let status = SecItemAdd(query as CFDictionary, nil)
         guard status == errSecSuccess else { throw KeychainError.saveFailed }
+#endif
     }
 
     static func read(for key: String) throws -> String {
+#if targetEnvironment(simulator)
+        guard let value = UserDefaults.standard.string(forKey: key) else {
+            throw KeychainError.readFailed
+        }
+        return value
+#else
         let query: [CFString: Any] = [
-            kSecClass:            kSecClassGenericPassword,
-            kSecAttrAccount:      key,
-            kSecAttrAccessible:   kSecAttrAccessibleAfterFirstUnlock,
-            kSecReturnData:       true,
-            kSecMatchLimit:       kSecMatchLimitOne
+            kSecClass:          kSecClassGenericPassword,
+            kSecAttrAccount:    key,
+            kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
+            kSecReturnData:     true,
+            kSecMatchLimit:     kSecMatchLimitOne
         ]
         var result: AnyObject?
         let status = SecItemCopyMatching(query as CFDictionary, &result)
-        if status == errSecItemNotFound { throw KeychainError.readFailed }
         guard status == errSecSuccess, let data = result as? Data else {
             throw KeychainError.readFailed
         }
         return String(decoding: data, as: UTF8.self)
+#endif
     }
 
     static func delete(for key: String) {
+#if targetEnvironment(simulator)
+        UserDefaults.standard.removeObject(forKey: key)
+#else
         let query: [CFString: Any] = [
             kSecClass:       kSecClassGenericPassword,
             kSecAttrAccount: key
         ]
         SecItemDelete(query as CFDictionary)
+#endif
     }
 }
